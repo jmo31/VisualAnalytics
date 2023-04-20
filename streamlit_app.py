8fabe4db34a298357fc2548508806581352988cc
jmo31 <103972084+jmo31@users.noreply.github.com>
2023-04-20 15:14:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import altair as alt
@@ -14,6 +13,8 @@ df1.index = df1['Year']
 cols = ['Winner', 'Runners-Up', 'Third', 'Fourth']
 df2 = df
 df2['Number of World Cups Won'] = df2.groupby('Winner')['Winner'].transform('count')
+df2.index = df2['Winner']
+df5=df2
 df3 = df
 #goals_scored = df3.groupby('Winner')['GoalsScored'].sum().nlargest(10)
 df3.index = df3['Country']
@@ -28,13 +29,13 @@ elif st.sidebar.checkbox('Countries finishes at the WorldCup'):
     st.bar_chart(df1[option])
 
 elif st.sidebar.checkbox('How many total WorldCups has each country won?'):
-    min_year = int(df['Year'].min())
-    max_year = int(df['Year'].max())
+    min_year = int(df5['Year'].min())
+    max_year = int(df5['Year'].max())
     year_range = st.slider('Select year range', min_value=min_year, max_value=max_year, value=(min_year,
                                                                                                max_year))
-    filtered_df = df2[(df2['Year'] >= year_range[0]) & (df2['Year'] <= year_range[1])]
-    fig = px.choropleth(filtered_df, locations='Country', locationmode='country names', color='Number of World Cups Won', 
-                    hover_name='Country', color_continuous_scale='Blues', range_color=(0, df['Number of World Cups Won'].max()))
+    filtered_df = df5[(df5['Year'] >= year_range[0]) & (df2['Year'] <= year_range[1])]
+    fig = px.choropleth(filtered_df, locations='Winner', locationmode='country names', color='Number of World Cups Won', 
+                    hover_name='Winner', color_continuous_scale='Blues', range_color=(0, df['Number of World Cups Won'].max()))
     fig.update_layout(title_text='World Cup Winners by Country', geo=dict(showframe=False, projection_type='equirectangular'))
     st.plotly_chart(fig)
 
